3f64f30fa1a6d5321961b7e4dd7536ee5e4e57dc
unknown <gabriel.panzenhagen@gmail.com>
2023-05-16 12:54:26 -0300

    lock

--- ventus-liber-app/vite.config.ts
+++ ventus-liber-app/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ventus-liber'
+  base: '/ventus-liber',
   plugins: [vue()],
   resolve: {
     alias: {
